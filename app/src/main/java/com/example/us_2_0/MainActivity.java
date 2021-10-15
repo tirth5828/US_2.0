ec3e3a6aba738c5556fa1b862d1b8358900d5370
Tirth <tirth5828@gmail.com>
2021-10-15 21:50:36 +0530

    Added time delay for animation to show

--- app/src/main/java/com/example/us_2_0/MainActivity.java
+++ app/src/main/java/com/example/us_2_0/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.us_2_0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import com.sawolabs.androidsdk.Sawo;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Handler H = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +18,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Log(View view) {
-
-        new Sawo(
-                this,
-                "1014c134-e723-43fe-a4d5-5933c554bf29", // your api key
-                "616975ed3148f7167607f1226AeBwjDLRISfuqNqGz6NqBS4"  // your api key secret
-        ).login(
-                "email", // can be one of 'email' or 'phone_number_sms'
-                LandingPage.class.getName()  // Callback class name
-        );
+        H.postDelayed(SAWO,5000);
     }
+
+    private Runnable SAWO = new Runnable() {
+        @Override
+        public void run() {
+            new Sawo(
+                    MainActivity.this,
+                    "1014c134-e723-43fe-a4d5-5933c554bf29", // your api key
+                    "616975ed3148f7167607f1226AeBwjDLRISfuqNqGz6NqBS4"  // your api key secret
+            ).login(
+                    "email", // can be one of 'email' or 'phone_number_sms'
+                    LandingPage.class.getName()  // Callback class name
+            );
+        }
+    };
 
 }
