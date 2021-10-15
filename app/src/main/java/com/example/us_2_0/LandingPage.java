174b36802c0ff34cbe7483d275e873a465a504aa
foram-patel-0201 <foramjpatel0201@gmail.com>
2021-10-16 00:26:11 +0530

    Navigation Drawer Added

--- app/src/main/java/com/example/us_2_0/LandingPage.java
+++ app/src/main/java/com/example/us_2_0/LandingPage.java
@@ -1,12 +1,108 @@
 package com.example.us_2_0;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class LandingPage extends AppCompatActivity {
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(LandingPage.this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        View view = navigationView.inflateHeaderView(R.layout.navigation_header);
+        navigationView.setItemIconTintList(null);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserItemSelected(item);
+                return false;
+            }
+        });
     }
-}
+
+
+    private void UserItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.share:
+
+                Intent myIntent = new Intent(Intent.ACTION_SEND);
+                myIntent.setType("text/plain");
+                String shareBody = "http://play.google.com/store/apps/details?id="+getPackageName();
+                String shareSub = " Thanks for Sharing";
+                myIntent.putExtra(Intent.EXTRA_SUBJECT,shareSub);
+                myIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+                startActivity(Intent.createChooser(myIntent,"Share using"));
+                break;
+
+            case R.id.more:
+
+
+                //onclick more
+                break;
+
+            case R.id.rate:
+
+                //onclick rate
+                break;
+
+            case R.id.privacy:
+
+                //onclick privacy
+                break;
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        else if (item.getItemId()==R.id.about){
+
+            //onclick for about
+
+        }else if (item.getItemId()==R.id.disclaimer){
+
+            //onclick for disclaimer
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.action_bar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    }
--- app/src/main/java/com/example/us_2_0/MainActivity.java
+++ app/src/main/java/com/example/us_2_0/MainActivity.java
@@ -1,20 +1,40 @@
 package com.example.us_2_0;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+
+import com.google.android.material.navigation.NavigationView;
 import com.sawolabs.androidsdk.Sawo;
 
 public class MainActivity extends AppCompatActivity {
-//Trying to figure out how things work
+
+
     private Handler H = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 
     public void Log(View view) {
