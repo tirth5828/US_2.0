034a81086b03234d3412ff13551c05c70d7384c8
foram-patel-0201 <foramjpatel0201@gmail.com>
2021-10-16 10:33:56 +0530

    Monthly and Weekly Calendar view added with manual event addition

--- app/src/main/java/com/example/us_2_0/LandingPage.java
+++ app/src/main/java/com/example/us_2_0/LandingPage.java
@@ -1,28 +1,49 @@
 package com.example.us_2_0;
 
+import static com.example.us_2_0.CalendarUtils.daysInMonthArray;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class LandingPage extends AppCompatActivity {
+import java.time.LocalDate;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class LandingPage extends AppCompatActivity implements CalendarAdapter.OnItemListener{
 
     private ActionBarDrawerToggle actionBarDrawerToggle;
 
+    private TextView monthYearText;
+    private RecyclerView calendarRecyclerView;
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+
+        initWidgets();
+        CalendarUtils.selectedDate = LocalDate.now();
+        setMonthView();
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -44,6 +65,11 @@ public class LandingPage extends AppCompatActivity {
                 return false;
             }
         });
+
+
+
+
+
     }
 
 
@@ -105,4 +131,61 @@ public class LandingPage extends AppCompatActivity {
         menuInflater.inflate(R.menu.action_bar_menu,menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    private void initWidgets()
+    {
+        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
+        monthYearText = findViewById(R.id.monthYearTV);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void setMonthView()
+    {
+        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
+        ArrayList<LocalDate> daysInMonth = daysInMonthArray(CalendarUtils.selectedDate);
+
+        CalendarAdapter calendarAdapter = new CalendarAdapter(daysInMonth, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
+        calendarRecyclerView.setLayoutManager(layoutManager);
+        calendarRecyclerView.setAdapter(calendarAdapter);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private String monthYearFromDate(LocalDate date)
+    {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM yyyy");
+        return date.format(formatter);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void previousMonthAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusMonths(1);
+        setMonthView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void nextMonthAction(View view)
+    {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusMonths(1);
+        setMonthView();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onItemClick(int position, LocalDate date)
+    {
+        if(date != null)
+        {
+            CalendarUtils.selectedDate = date;
+            setMonthView();
+        }
+    }
+
+
+    public void weeklyAction(View view)
+    {
+        startActivity(new Intent(this, WeekViewActivity.class));
+    }
+
+}
