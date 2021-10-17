0244f7ea2c858cd0faae58a42de10bb32f9b4eb7
Tirth <tirth5828@gmail.com>
2021-10-17 11:19:39 +0530

    Connected Notification Page with API

--- app/src/main/java/com/example/us_2_0/Notification.java
+++ app/src/main/java/com/example/us_2_0/Notification.java
@@ -1,23 +1,142 @@
 package com.example.us_2_0;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Notification extends AppCompatActivity {
+    TextView T1,T2,T3,T4,T5,T6,T7;
 
-
+    int max_id = 29;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
 
+        getlength ();
+
+
+        T1 = findViewById (R.id.textView4);
+        getresponse (T1,max_id);
+
+        T2 = findViewById (R.id.textView5);
+        getresponse (T2,max_id-1);
+
+        T3 = findViewById (R.id.textView6);
+        getresponse (T3,max_id-2);
+
+        T4 = findViewById (R.id.textView7);
+        getresponse (T4,max_id-3);
+
+        T5 = findViewById (R.id.textView8);
+        getresponse (T5,max_id-4);
+
+        T6 = findViewById (R.id.textView9);
+        getresponse (T6,max_id-5);
+
+        T7 = findViewById (R.id.textView10);
+        getresponse (T7,max_id-6);
+
     }
 
     public void ChangeText(View view) {
-        final TextView helloTextView = (TextView) findViewById(R.id.textView4);
-        helloTextView.setText(R.string.N1);
+        getlength ();
+
+        T1 = findViewById (R.id.textView4);
+        getresponse (T1,max_id);
+
+        T2 = findViewById (R.id.textView5);
+        getresponse (T2,max_id-1);
+
+        T3 = findViewById (R.id.textView6);
+        getresponse (T3,max_id-2);
+
+        T4 = findViewById (R.id.textView7);
+        getresponse (T4,max_id-3);
+
+        T5 = findViewById (R.id.textView8);
+        getresponse (T5,max_id-4);
+
+        T6 = findViewById (R.id.textView9);
+        getresponse (T6,max_id-5);
+
+        T7 = findViewById (R.id.textView10);
+        getresponse (T7,max_id-6);
     }
+
+    public void getresponse(TextView T, int id){
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue (this);
+
+        String url = "https://x8ki-letl-twmt.n7.xano.io/api:WMKQkzzw/Important_Dates/"
+                + id;
+
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest (Request.Method.GET,
+                url, null, new Response.Listener<JSONObject> () {
+            @Override
+            public void onResponse (JSONObject response) {
+                try {
+
+                    String Res = response.getString ("Name") + " is scheduled on "
+                            + response.getString ("Date") +", "
+                            +response.getString ("Day");
+                    T.setText (Res);
+
+                } catch (JSONException e) {
+                    e.printStackTrace ();
+                }
+            }
+        }, new Response.ErrorListener () {
+            @Override
+            public void onErrorResponse (VolleyError error) {
+                Log.d("My APP","Something Went Wrong");
+            }
+        });
+
+        requestQueue.add (jsonObjectRequest);
+    }
+
+
+    public void getlength(){
+        RequestQueue requestQueue;
+        requestQueue = Volley.newRequestQueue (this);
+
+        String url = "https://x8ki-letl-twmt.n7.xano.io/api:WMKQkzzw/Important_Dates";
+
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest (Request.Method.GET,
+                url, null, new Response.Listener<JSONArray> () {
+            @Override
+            public void onResponse (JSONArray response) {
+
+                max_id = response.length ();
+
+            }
+        }, new Response.ErrorListener () {
+            @Override
+            public void onErrorResponse (VolleyError error) {
+                Log.d("My APP","Something Went Wrong");
+            }
+        });
+
+        requestQueue.add (jsonArrayRequest);
+    }
+
 }
--- app/src/main/java/com/example/us_2_0/WeekViewActivity.java
+++ app/src/main/java/com/example/us_2_0/WeekViewActivity.java
@@ -1,9 +1,7 @@
 package com.example.us_2_0;
 
 
-
 import static com.example.us_2_0.CalendarUtils.daysInWeekArray;
-import static com.example.us_2_0.CalendarUtils.formattedTime;
 import static com.example.us_2_0.CalendarUtils.monthYearFromDate;
 
 import androidx.annotation.RequiresApi;
@@ -13,7 +11,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -23,205 +20,192 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.Toolbar;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.ArrayList;
 
-public class WeekViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener
-{
+public class WeekViewActivity extends AppCompatActivity implements CalendarAdapter.OnItemListener {
     private TextView monthYearText;
     private RecyclerView calendarRecyclerView;
     private ListView eventListView;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_week_view);
-        initWidgets();
-        CalendarUtils.selectedDate = LocalDate.now();
-        setWeekView();
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_week_view);
+        initWidgets ();
+        CalendarUtils.selectedDate = LocalDate.now ();
+        setWeekView ();
 
 
-        eventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        eventListView.setOnItemClickListener (new AdapterView.OnItemClickListener () {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position==0){
-                    Uri uri = Uri.parse("https://classroom.google.com/c/MjQwNjEwNzU0NzIy/a/MjQwNjEyNjY5NTQ2/details");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                    Toast.makeText(getApplicationContext(), "This is just a demo link", Toast.LENGTH_SHORT).show();
-                } else if(position==1){
-                    Uri uri = Uri.parse("https://betamoodle.iiitvadodara.ac.in/login/index.php");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                    Toast.makeText(getApplicationContext(), "This is just a demo link", Toast.LENGTH_SHORT).show();
+            public void onItemClick (AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    Uri uri = Uri.parse ("https://classroom.google.com/c/MjQwNjEwNzU0NzIy/a/MjQwNjEyNjY5NTQ2/details");
+                    Intent intent = new Intent (Intent.ACTION_VIEW, uri);
+                    startActivity (intent);
+                    Toast.makeText (getApplicationContext (), "This is just a demo link", Toast.LENGTH_SHORT).show ();
+                } else if (position == 1) {
+                    Uri uri = Uri.parse ("https://betamoodle.iiitvadodara.ac.in/login/index.php");
+                    Intent intent = new Intent (Intent.ACTION_VIEW, uri);
+                    startActivity (intent);
+                    Toast.makeText (getApplicationContext (), "This is just a demo link", Toast.LENGTH_SHORT).show ();
                 } else {
-                    Uri uri = Uri.parse("https://meet.google.com/trp-rnxf-uzm");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                    Toast.makeText(getApplicationContext(), "This is just a demo link", Toast.LENGTH_SHORT).show();
+                    Uri uri = Uri.parse ("https://meet.google.com/trp-rnxf-uzm");
+                    Intent intent = new Intent (Intent.ACTION_VIEW, uri);
+                    startActivity (intent);
+                    Toast.makeText (getApplicationContext (), "This is just a demo link", Toast.LENGTH_SHORT).show ();
                 }
             }
         });
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        if (!prefs.getBoolean("onlyonce", false)) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences (this);
+        if (!prefs.getBoolean ("onlyonce", false)) {
 
-        Event sampleEvent = new Event("Maths Assignment 1",LocalDate.of(2021,10,16), LocalTime.of(12,00,00));
-        Event.eventsList.add(sampleEvent);
-
-
-        Event sampleEvent1 = new Event("Physics Assignment 2",LocalDate.of(2021,10,16), LocalTime.of(12,00,00));
-        Event.eventsList.add(sampleEvent1);
+            Event sampleEvent = new Event ("Maths Assignment 1", LocalDate.of (2021, 10, 16), LocalTime.of (12, 00, 00));
+            Event.eventsList.add (sampleEvent);
 
 
-        Event sampleEvent2 = new Event("Electronics Assignment 2",LocalDate.of(2021,10,16), LocalTime.of(12,00,00));
-        Event.eventsList.add(sampleEvent2);
+            Event sampleEvent1 = new Event ("Physics Assignment 2", LocalDate.of (2021, 10, 16), LocalTime.of (12, 00, 00));
+            Event.eventsList.add (sampleEvent1);
 
 
-        Event sampleEvent3 = new Event("Maths Assignment 2",LocalDate.of(2021,10,18), LocalTime.of(12,00,00));
-        Event.eventsList.add(sampleEvent3);
+            Event sampleEvent2 = new Event ("Electronics Assignment 2", LocalDate.of (2021, 10, 16), LocalTime.of (12, 00, 00));
+            Event.eventsList.add (sampleEvent2);
 
 
-        Event sampleEvent4 = new Event("Electronics Assignment 3",LocalDate.of(2021,10,20), LocalTime.of(12,00,00));
-        Event.eventsList.add(sampleEvent4);
+            Event sampleEvent3 = new Event ("Maths Assignment 2", LocalDate.of (2021, 10, 18), LocalTime.of (12, 00, 00));
+            Event.eventsList.add (sampleEvent3);
 
 
-        Event sampleEvent5 = new Event("Physics Assignment 3",LocalDate.of(2021,10,17), LocalTime.of(20,00,00));
-        Event.eventsList.add(sampleEvent5);
+            Event sampleEvent4 = new Event ("Electronics Assignment 3", LocalDate.of (2021, 10, 20), LocalTime.of (12, 00, 00));
+            Event.eventsList.add (sampleEvent4);
 
 
-        Event sampleEvent6 = new Event("Maths Assignment 3",LocalDate.of(2021,10,24), LocalTime.of(12,00,00));
-        Event.eventsList.add(sampleEvent6);
+            Event sampleEvent5 = new Event ("Physics Assignment 3", LocalDate.of (2021, 10, 17), LocalTime.of (20, 00, 00));
+            Event.eventsList.add (sampleEvent5);
 
 
-        Event sampleEvent7 = new Event("Electronics Quiz 1",LocalDate.of(2021,10,23), LocalTime.of(10,00,00));
-        Event.eventsList.add(sampleEvent7);
+            Event sampleEvent6 = new Event ("Maths Assignment 3", LocalDate.of (2021, 10, 24), LocalTime.of (12, 00, 00));
+            Event.eventsList.add (sampleEvent6);
 
 
-        Event sampleEvent8 = new Event("Physics Quiz 1",LocalDate.of(2021,10,21), LocalTime.of(10,00,00));
-        Event.eventsList.add(sampleEvent8);
+            Event sampleEvent7 = new Event ("Electronics Quiz 1", LocalDate.of (2021, 10, 23), LocalTime.of (10, 00, 00));
+            Event.eventsList.add (sampleEvent7);
 
 
-        Event sampleEvent9 = new Event("Maths Quiz 1",LocalDate.of(2021,10,22), LocalTime.of(10,00,00));
-        Event.eventsList.add(sampleEvent9);
+            Event sampleEvent8 = new Event ("Physics Quiz 1", LocalDate.of (2021, 10, 21), LocalTime.of (10, 00, 00));
+            Event.eventsList.add (sampleEvent8);
 
 
-        Event sampleEvent10 = new Event("Maths lecture",LocalDate.of(2021,10,14), LocalTime.of(8,00,00));
-        Event.eventsList.add(sampleEvent10);
-
-        Event sampleEvent11 = new Event("Electronics lecture",LocalDate.of(2021,10,14), LocalTime.of(10,00,10));
-        Event.eventsList.add(sampleEvent11);
+            Event sampleEvent9 = new Event ("Maths Quiz 1", LocalDate.of (2021, 10, 22), LocalTime.of (10, 00, 00));
+            Event.eventsList.add (sampleEvent9);
 
 
-        Event sampleEvent12 = new Event("Physics lecture",LocalDate.of(2021,10,15), LocalTime.of(14,30,00));
-        Event.eventsList.add(sampleEvent12);
+            Event sampleEvent10 = new Event ("Maths lecture", LocalDate.of (2021, 10, 14), LocalTime.of (8, 00, 00));
+            Event.eventsList.add (sampleEvent10);
+
+            Event sampleEvent11 = new Event ("Electronics lecture", LocalDate.of (2021, 10, 14), LocalTime.of (10, 00, 10));
+            Event.eventsList.add (sampleEvent11);
 
 
-        Event sampleEvent13 = new Event("Maths lecture",LocalDate.of(2021,10,15), LocalTime.of(16,00,00));
-        Event.eventsList.add(sampleEvent13);
-
-        Event sampleEvent14 = new Event("Electronics lecture",LocalDate.of(2021,10,13), LocalTime.of(8,30,00));
-        Event.eventsList.add(sampleEvent14);
+            Event sampleEvent12 = new Event ("Physics lecture", LocalDate.of (2021, 10, 15), LocalTime.of (14, 30, 00));
+            Event.eventsList.add (sampleEvent12);
 
 
-        Event sampleEvent15 = new Event("Physics lecture",LocalDate.of(2021,10,13), LocalTime.of(10,00,00));
-        Event.eventsList.add(sampleEvent15);
+            Event sampleEvent13 = new Event ("Maths lecture", LocalDate.of (2021, 10, 15), LocalTime.of (16, 00, 00));
+            Event.eventsList.add (sampleEvent13);
+
+            Event sampleEvent14 = new Event ("Electronics lecture", LocalDate.of (2021, 10, 13), LocalTime.of (8, 30, 00));
+            Event.eventsList.add (sampleEvent14);
 
 
-        Event sampleEvent16 = new Event("Maths lecture",LocalDate.of(2021,10,24), LocalTime.of(10,10,10));
-        Event.eventsList.add(sampleEvent16);
-
-        Event sampleEvent17 = new Event("Electronics lecture",LocalDate.of(2021,10,24), LocalTime.of(10,10,10));
-        Event.eventsList.add(sampleEvent17);
+            Event sampleEvent15 = new Event ("Physics lecture", LocalDate.of (2021, 10, 13), LocalTime.of (10, 00, 00));
+            Event.eventsList.add (sampleEvent15);
 
 
-        Event sampleEvent18 = new Event("Physics lecture",LocalDate.of(2021,10,12), LocalTime.of(8,30,00));
-        Event.eventsList.add(sampleEvent18);
+            Event sampleEvent16 = new Event ("Maths lecture", LocalDate.of (2021, 10, 24), LocalTime.of (10, 10, 10));
+            Event.eventsList.add (sampleEvent16);
+
+            Event sampleEvent17 = new Event ("Electronics lecture", LocalDate.of (2021, 10, 24), LocalTime.of (10, 10, 10));
+            Event.eventsList.add (sampleEvent17);
 
 
-        Event sampleEvent19 = new Event("Maths lecture",LocalDate.of(2021,10,12), LocalTime.of(10,00,00));
-        Event.eventsList.add(sampleEvent19);
-
-        Event sampleEvent20 = new Event("Electronics lecture",LocalDate.of(2021,10,19), LocalTime.of(10,10,10));
-        Event.eventsList.add(sampleEvent20);
+            Event sampleEvent18 = new Event ("Physics lecture", LocalDate.of (2021, 10, 12), LocalTime.of (8, 30, 00));
+            Event.eventsList.add (sampleEvent18);
 
 
-        Event sampleEvent21 = new Event("Physics lecture",LocalDate.of(2021,10,19), LocalTime.of(10,10,10));
-        Event.eventsList.add(sampleEvent21);
+            Event sampleEvent19 = new Event ("Maths lecture", LocalDate.of (2021, 10, 12), LocalTime.of (10, 00, 00));
+            Event.eventsList.add (sampleEvent19);
 
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putBoolean("onlyonce", true);
-            editor.commit();
+            Event sampleEvent20 = new Event ("Electronics lecture", LocalDate.of (2021, 10, 19), LocalTime.of (10, 10, 10));
+            Event.eventsList.add (sampleEvent20);
+
+
+            Event sampleEvent21 = new Event ("Physics lecture", LocalDate.of (2021, 10, 19), LocalTime.of (10, 10, 10));
+            Event.eventsList.add (sampleEvent21);
+
+            SharedPreferences.Editor editor = prefs.edit ();
+            editor.putBoolean ("onlyonce", true);
+            editor.commit ();
         }
     }
 
-    private void initWidgets()
-    {
-        calendarRecyclerView = findViewById(R.id.calendarRecyclerView);
-        monthYearText = findViewById(R.id.monthYearTV);
-        eventListView = findViewById(R.id.eventListView);
+    private void initWidgets () {
+        calendarRecyclerView = findViewById (R.id.calendarRecyclerView);
+        monthYearText = findViewById (R.id.monthYearTV);
+        eventListView = findViewById (R.id.eventListView);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    private void setWeekView()
-    {
-        monthYearText.setText(monthYearFromDate(CalendarUtils.selectedDate));
-        ArrayList<LocalDate> days = daysInWeekArray(CalendarUtils.selectedDate);
+    private void setWeekView () {
+        monthYearText.setText (monthYearFromDate (CalendarUtils.selectedDate));
+        ArrayList<LocalDate> days = daysInWeekArray (CalendarUtils.selectedDate);
 
-        CalendarAdapter calendarAdapter = new CalendarAdapter(days, this);
-        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 7);
-        calendarRecyclerView.setLayoutManager(layoutManager);
-        calendarRecyclerView.setAdapter(calendarAdapter);
-        setEventAdpater();
+        CalendarAdapter calendarAdapter = new CalendarAdapter (days, this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager (getApplicationContext (), 7);
+        calendarRecyclerView.setLayoutManager (layoutManager);
+        calendarRecyclerView.setAdapter (calendarAdapter);
+        setEventAdpater ();
     }
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public void previousWeekAction(View view)
-    {
-        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks(1);
-        setWeekView();
+    public void previousWeekAction (View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.minusWeeks (1);
+        setWeekView ();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public void nextWeekAction(View view)
-    {
-        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks(1);
-        setWeekView();
+    public void nextWeekAction (View view) {
+        CalendarUtils.selectedDate = CalendarUtils.selectedDate.plusWeeks (1);
+        setWeekView ();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    public void onItemClick(int position, LocalDate date)
-    {
+    public void onItemClick (int position, LocalDate date) {
         CalendarUtils.selectedDate = date;
-        setWeekView();
+        setWeekView ();
     }
 
     @Override
-    protected void onResume()
-    {
-        super.onResume();
-        setEventAdpater();
+    protected void onResume () {
+        super.onResume ();
+        setEventAdpater ();
     }
 
-    private void setEventAdpater()
-    {
-        ArrayList<Event> dailyEvents = Event.eventsForDate(CalendarUtils.selectedDate);
-        EventAdapter eventAdapter = new EventAdapter(getApplicationContext(), dailyEvents);
-        eventListView.setAdapter(eventAdapter);
+    private void setEventAdpater () {
+        ArrayList<Event> dailyEvents = Event.eventsForDate (CalendarUtils.selectedDate);
+        EventAdapter eventAdapter = new EventAdapter (getApplicationContext (), dailyEvents);
+        eventListView.setAdapter (eventAdapter);
     }
 
-    public void newEventAction(View view)
-    {
-        startActivity(new Intent(this, EventEditActivity.class));
+    public void newEventAction (View view) {
+        startActivity (new Intent (this, EventEditActivity.class));
     }
-
 
 
 }
